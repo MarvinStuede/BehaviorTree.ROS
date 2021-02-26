1fe75f7bc4d5e48bf9d1f2d64d07afd1cc94db71
Marvin Stuede <Marvin.Stuede@imes.uni-hannover.de>
2021-02-26 18:32:19 +0100

    Comments for ROS logger, removes cpp

--- include/behaviortree_ros/loggers/ros_topic_logger.h
+++ include/behaviortree_ros/loggers/ros_topic_logger.h
@@ -12,6 +12,60 @@
 namespace BT
 {
 
+int8_t toROS(NodeType type){
+  switch (type)
+  {
+  case NodeType::ACTION:
+    return behaviortree_ros::TreeNode::ACTION;
+  case NodeType::CONDITION:
+    return behaviortree_ros::TreeNode::CONDITION;
+  case NodeType::DECORATOR:
+    return behaviortree_ros::TreeNode::DECORATOR;
+  case NodeType::CONTROL:
+    return behaviortree_ros::TreeNode::CONTROL;
+  case NodeType::SUBTREE:
+    return behaviortree_ros::TreeNode::SUBTREE;
+  default:
+    return behaviortree_ros::TreeNode::UNDEFINED;
+  }
+}
+
+int8_t toROS(NodeStatus status){
+  switch (status)
+  {
+  case NodeStatus::IDLE:
+    return behaviortree_ros::NodeStatus::IDLE;
+  case NodeStatus::RUNNING:
+    return behaviortree_ros::NodeStatus::RUNNING;
+  case NodeStatus::FAILURE:
+    return behaviortree_ros::NodeStatus::FAILURE;
+  default:
+    return behaviortree_ros::NodeStatus::IDLE;
+  }
+}
+
+// Convert the Tree to a ROS message
+behaviortree_ros::BehaviorTree BTtoROS(const BT::Tree & tree){
+  behaviortree_ros::BehaviorTree ros_tree;
+  behaviortree_ros::TreeNode ros_node;
+
+  ros_tree.root_uid = tree.rootNode()->UID();
+
+  for (const auto &node : tree.nodes){
+    ros_node.uid = node->UID();
+    ros_node.type = toROS(node->type());
+    ros_node.instance_name = node->name();
+    ros_node.registration_name = node->registrationName();
+    //Following values not implemented
+    //ros_node.params = ...
+    //ros_node.status = ...
+    //ros_node.children_uid = ...
+
+    ros_tree.nodes.push_back(ros_node);
+  }
+  return ros_tree;
+}
+
 class RosTopicLogger : public BT::StatusChangeLogger
 {
 public:
@@ -36,18 +90,21 @@ public:
     int32_t t_nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(timestamp).count();
     event.timestamp = ros::Time(t_sec, t_nsec);
 
-    ros_status.value = this->toROS(status);
+    ros_status.value = toROS(status);
     event.status = ros_status;
-    ros_status.value = this->toROS(prev_status);
+    ros_status.value = toROS(prev_status);
     event.prev_status = ros_status;
 
     event.uid = node.UID();
     event_log_.state_changes.push_back(event);
 
   }
-
+  /**
+   * @brief Write the current BT to the ROS message to publish
+   * @param tree
+   */
   void writeBT(const BT::Tree &tree){
-    event_log_.behavior_tree = this->BTtoROS(tree);
+    event_log_.behavior_tree = BTtoROS(tree);
   }
 
   void flush() override
@@ -63,58 +120,6 @@ protected:
   ros::NodeHandle& node_;
   ros::Publisher publisher_;
   behaviortree_ros::StatusChangeLog event_log_;
-
-
-  behaviortree_ros::BehaviorTree BTtoROS(const BT::Tree & tree){
-    behaviortree_ros::BehaviorTree ros_tree;
-    behaviortree_ros::TreeNode ros_node;
-
-    ros_tree.root_uid = tree.rootNode()->UID();
-
-    for (const auto &node : tree.nodes){
-      ros_node.uid = node->UID();
-      ros_node.type = this->toROS(node->type());
-      //ros_node.params = ...
-      //ros_node.status = this->toROS(node->status());
-      //ros_node.children_uid = ...
-      ros_node.instance_name = node->name();
-      ros_node.registration_name = node->registrationName();
-      ros_tree.nodes.push_back(ros_node);
-    }
-    return ros_tree;
-  }
-
-  int8_t toROS(NodeType type){
-    switch (type)
-    {
-    case NodeType::ACTION:
-      return behaviortree_ros::TreeNode::ACTION;
-    case NodeType::CONDITION:
-      return behaviortree_ros::TreeNode::CONDITION;
-    case NodeType::DECORATOR:
-      return behaviortree_ros::TreeNode::DECORATOR;
-    case NodeType::CONTROL:
-      return behaviortree_ros::TreeNode::CONTROL;
-    case NodeType::SUBTREE:
-      return behaviortree_ros::TreeNode::SUBTREE;
-    default:
-      return behaviortree_ros::TreeNode::UNDEFINED;
-    }
-  }
-
-  int8_t toROS(NodeStatus status){
-    switch (status)
-    {
-    case NodeStatus::IDLE:
-      return behaviortree_ros::NodeStatus::IDLE;
-    case NodeStatus::RUNNING:
-      return behaviortree_ros::NodeStatus::RUNNING;
-    case NodeStatus::FAILURE:
-      return behaviortree_ros::NodeStatus::FAILURE;
-    default:
-      return behaviortree_ros::NodeStatus::IDLE;
-    }
-  }
 
 };
 
